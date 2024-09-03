2a164f81ab83595dcf78248717b2097932a7ce32
@sugimotokaz <aaddggjjmmpppttww@gmail.com>
2024-09-03 20:58:16 +0900

    記事作成ページ、記事詳細ページ再コミット

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,13 +3,13 @@ class Article < ApplicationRecord
   belongs_to :user
   belongs_to :oshi_name
 
+  enum category: { others: 0, impression: 1, introduction: 2 }
+  enum visible_gender: { not_selected: 0, male: 1, female: 2 }
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :notice, length: { maximum: 100 }
   validates :category, presence: true
   validates :visible_gender, presence: true
   validates :visible_oshi, inclusion: { in: [true, false] }
-
-  enum category: { others: 0, impression: 1, introduction: 2 }
-  enum visible_gender: { not_selected: 0, male: 1, female: 2 }
 
 end
