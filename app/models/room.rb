fedd522a5f4b88f57219f1bb9be00d99a4c5b966
@sugimotokaz <aaddggjjmmpppttww@gmail.com>
2024-11-18 17:53:26 +0900

    テーブル、モデルの設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :user_rooms, dependent: :destroy
   has_many :users, through: :user_rooms
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
 end
