711d732b8b96fb5a0d21bfc3d24d207e68461655
@sugimotokaz <aaddggjjmmpppttww@gmail.com>
2024-12-03 00:37:25 +0900

    models/user_specの作成、simplecovの導入

--- Gemfile
+++ Gemfile
@@ -96,4 +96,6 @@ gem 'enum_help'
 group :development, :test do
   gem 'rspec-rails'
   gem 'factory_bot_rails'
+
+  gem 'simplecov'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'ユーザー新規登録' do
+    it '全てのフォームに入力されていば正常に保存されるか' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it '名前の入力がない場合、バリデーションが機能するか' do
+      user = build(:user, name: "")
+      expect(user).to be_invalid
+    end
+
+    it '名前の文字数が15文字以内でない場合、バリデーションが機能するか' do
+      user = build(:user, name: 'a' * 16)
+      expect(user).to be_invalid
+    end
+
+    it 'メールアドレスがない場合、バリデーションが機能するか' do
+      user = build(:user, email: "")
+      expect(user).to be_invalid
+    end
+
+    it 'メールアドレスは一意であること' do
+      create(:user, email: 'test@example.com')
+      user = build(:user, email: 'test@example.com')
+      expect(user).to be_invalid
+    end
+
+    it 'パスワードがない場合、バリデーションが機能するか' do
+      user = build(:user, password: "")
+      expect(user).to be_invalid
+    end
+
+    it '確認用パスワードがない場合' do
+      user = build(:user, password_confirmation: "")
+      expect(user).to be_invalid
+    end
+  end
 end
