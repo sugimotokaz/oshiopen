acae9829edfa01c85411aeccfaf06f582f0a4ca9
@sugimotokaz <aaddggjjmmpppttww@gmail.com>
2025-03-10 22:55:47 +0900

    編集ページに移動した際にも正常にカウントされるように設定

--- app/javascript/character_counter.js
+++ app/javascript/character_counter.js
@@ -1,44 +1,51 @@
-function keyUpTitle(e) {
-  let str = inputTextTitle.value;
-  // 改行で文字のカウントが起きない設定。改行を空白に置き換えてくれるため、カウントされない。
-  str = str.replace(/\r?\n/g, '');
-
-  // カウント数を減らす記述。
-  let num = 50 - str.length;
-  // ID属性を取得して、現在のカウント数を代入する記述。
+document.addEventListener("turbo:load", function () {
+  // タイトルの文字数カウント
+  const inputTextTitle = document.getElementById('inputTextTitle');
   const characterCountTitle = document.getElementById('characterCountTitle');
-  characterCountTitle.textContent = num;
-  // 文字数が0（最大値をオーバー）してしまうと警告として赤文字になるように設定。
   const characterCountWrapTitle = document.getElementById('characterCountWrapTitle');
-  if(num >= 0) {
-    characterCountWrapTitle.style.color = 'black';
-  } else {
-    characterCountWrapTitle.style.color = 'red';
+
+  if (inputTextTitle) {
+    function keyUpTitle() {
+      let str = inputTextTitle.value.replace(/\r?\n/g, '');
+      let num = 50 - str.length;
+
+      characterCountTitle.textContent = num;
+      if(num >= 0) {
+        characterCountWrapTitle.style.color = 'black';
+      } else {
+        characterCountWrapTitle.style.color = 'red';
+      }
+    }
+
+    // **ページロード時にカウントを更新**
+    keyUpTitle();
+
+    // **キー入力時にカウントを更新**
+    inputTextTitle.addEventListener('keyup', keyUpTitle);
   }
-}
 
-const inputTextTitle = document.getElementById('inputTextTitle');
-inputTextTitle.addEventListener('keyup', keyUpTitle, false);
-
-
-function keyUpNotice(e) {
-  let str = inputTextNotice.value;
-  // 改行で文字のカウントが起きない設定。改行を空白に置き換えてくれるため、カウントされない。
-  str = str.replace(/\r?\n/g, '');
-
-  // カウント数を減らす記述。
-  let num = 100 - str.length;
-  // ID属性を取得して、現在のカウント数を代入する記述。
+  // お知らせの文字数カウント
+  const inputTextNotice = document.getElementById('inputTextNotice');
   const characterCountNotice = document.getElementById('characterCountNotice');
-  characterCountNotice.textContent = num;
-  // 文字数が0（最大値をオーバー）してしまうと警告として赤文字になるように設定。
   const characterCountWrapNotice = document.getElementById('characterCountWrapNotice');
-  if(num >= 0) {
-    characterCountWrapNotice.style.color = 'black';
-  } else {
-    characterCountWrapNotice.style.color = 'red';
-  }
-}
 
-const inputTextNotice = document.getElementById('inputTextNotice');
-inputTextNotice.addEventListener('keyup', keyUpNotice, false);
+  if (inputTextNotice) {
+    function keyUpNotice() {
+      let str = inputTextNotice.value.replace(/\r?\n/g, '');
+      let num = 100 - str.length;
+
+      characterCountNotice.textContent = num;
+      if(num >= 0) {
+        characterCountWrapNotice.style.color = 'black';
+      } else {
+        characterCountWrapNotice.style.color = 'red';
+      }
+    }
+
+    // **ページロード時にカウントを更新**
+    keyUpNotice();
+
+    // **キー入力時にカウントを更新**
+    inputTextNotice.addEventListener('keyup', keyUpNotice);
+  }
+});
